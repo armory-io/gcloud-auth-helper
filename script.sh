cdc792624927bf8dcca27d12942ed6d718f24fd3
Ezequiel Rosas <ezequiel.rosas@digitalonus.com>
2020-10-23 00:58:16 -0500

    Use exec & rm to prevent reopening same fd

--- script.sh
+++ script.sh
@@ -4,13 +4,18 @@ set -uo pipefail
 
 : EXEC_CREDENTIAL_PATH ${EXEC_CREDENTIAL_PATH:=/tmp/gcloud/auth_token};
 
-if ! test -p "$EXEC_CREDENTIAL_PATH"; then
+_cleanup() { exec 3>&- ; exit 0 ; };
+for i in TERM QUIT INT HUP EXIT; do trap '_cleanup' $i; done;
+
+if ! test -e "$EXEC_CREDENTIAL_PATH"; then
   mkfifo "$EXEC_CREDENTIAL_PATH";
 fi;
 while test -p "$EXEC_CREDENTIAL_PATH"; do
-  (
-    echo "$(date): Generating token" >&2;
-    script -q -f -c /app/gcp-exec-creds;
-  ) > "$EXEC_CREDENTIAL_PATH" ;
+  exec 3>"$EXEC_CREDENTIAL_PATH";
+  rm -rf "$EXEC_CREDENTIAL_PATH" > /dev/null;
+  mkfifo "$EXEC_CREDENTIAL_PATH" > /dev/null;
+  echo "$(date): Generating token" >&2;
+  /app/gcp-exec-creds >&3;
+  exec 3>&- ;
 done;
 
